d1f8b879df2b7469efbb0f79f1524de043249902
Yash-Gupta <yash0314@gmail.com>
2017-12-25 18:54:25 -0800

    added product detail

--- src/scenes/ProductDetail/components/ProductDetail.jsx
+++ src/scenes/ProductDetail/components/ProductDetail.jsx
@@ -47,36 +47,88 @@ class ProductDetail extends Component {
 			<div>
 				<div className="container">
 					<div className="row home">
-						<div className="col-md-6">
+						{/*<div className = "prodHeader"><p className="product prodTitle">{this.state.name}</p> <hr /></div>*/}
+						<div className="col-md-8">
 							<p>
-								<div className ="col-md-12 " id = "">
-									<div className = "img-holder" style={backgroundStyles}></div>
+								{/*<div className ="col-md-9  " id = "" >
+									<div className = "img-holder" style={backgroundStyles} ></div>
+								</div>*/}
+								<div className = "col-md-9">
+									<div className = "img-holder">
+									</div>
+								</div>
+								
+								<div className = "col-md-3">
+									<div className = "preview">
+									</div>
+									<div className = "preview">
+									</div>
+									<div className = "preview">
+									</div>
 								</div>
 							</p>
 						</div>
-						<div className="col-md-6 ">
+						<div className="col-md-4 ">
 							<p>
 								<div className ="col-md-12 " id = "">
-									<p className="product prodTitle">Name: {this.state.name}</p>
-									<p className="product prodDesc">Description: {this.state.description}</p>
-									<p className="product prodAuthor">Author: {this.state.author}</p>
-									<p className= "product prodSize">Size: {this.state.size}</p>
-									<p className= "product prodPrice">Price: {this.state.price}</p>
+									<p className = "prodTitle">{this.state.name}</p>
+									{/*<p className="product prodDesc"> <span className = "description">DESCRIPTION:</span> {this.state.description}</p>*/}
+									{/*<p className="product prodAuthor">Author: {this.state.author}</p>*/}
+									{/*<p className = "product prodSize"><span className = "description">CATEGORIES:</span>  <span className = "categ" ><i> {this.state.category}</i></span></p>*/}
+									<p className= "product prodSize"><span className = "description">SIZE:</span> {this.state.size}</p>
+									<p className= "product prodPrice">{this.state.price}</p>
 									
+								
+									<button className = "buyBtn">BUY</button>
+									<button className = "messageBtn">MESSAGE</button>
+
+									<div className = "profileHolder">
+										<div className = "profImg">
+
+										</div>
+										<div className = "profContent">
+											<p className = "profUser">AgentYash01</p>
+											<p className = "profLoc">Chandigargh</p>
+											<div className = "rating">
+												<div className = "star">
+												</div>
+												<div className = "star">
+												</div>
+												<div className = "star">
+												</div>
+												<div className = "star">
+												</div>
+												<div className = "starOutline">
+												</div>
+											</div>
+
+										</div>
+									</div>									
 			
-									<div className = "buyBtn">
-										<p className = "buyit">BUY</p>
-									</div>
+							
 			
-									<hr />
-			
-									<p className = "useless">Category: {this.state.category}</p>
-									<p className = "useless">Id: {this.state.id}</p>
+									
+									{/*<p className = "useless">Id: {this.state.id}</p>*/}
 								</div>
 							</p>
 						</div>
 					</div>
+
+					<div className = " row ">
+						<div className = "col-md-12 ">
+							<div className = "descrip">
+								<h1 className = "desname">Description</h1>
+								<p className = "desPar">{this.state.description}</p>
+							</div>
+							
+						</div>
+						
+					</div>
 				</div>
+
+				<hr />
+
+				<center><h1 className = "relList">Related Listings</h1></center>
 			</div>
 
 		);
